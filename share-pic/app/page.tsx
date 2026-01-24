50359fcb6a5561fc617aaecbf07f22b1250e07a1
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-01-24 12:05:36 +0100

    Create a simple form on the main page

--- share-pic/app/page.tsx
+++ share-pic/app/page.tsx
@@ -1,20 +1,43 @@
-import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
+import { Button } from '@/components/ui/button';
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import Form from 'next/form';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-4xl'>Upload an image you want to share</h1>
+    <main className='w-full h-screen bg-gray-100 pt-16'>
+      <div className='mx-auto max-w-4xl bg-white pt-20 px-8 flex flex-col gap-16'>
+        <h1 className='text-2xl'>Upload an image you want to share</h1>
 
-      <Form action='/result'>
-        <FieldGroup>
-          <Field>
-            <FieldLabel htmlFor='author'>Your name</FieldLabel>
-            <Input id='author' name='author' autoComplete='off' />
-          </Field>
-        </FieldGroup>
-      </Form>
+        <Form action='/result' className='flex flex-col gap-8 mb-8'>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor='author'>Your name</FieldLabel>
+              <FieldDescription>
+                This is the which will be displayed next to the image.
+              </FieldDescription>
+              <Input id='author' name='author' autoComplete='off' />
+            </Field>
+
+            <Field>
+              <FieldLabel htmlFor='description'>Description</FieldLabel>
+              <FieldDescription>
+                If you want to tell something about your image, but this is
+                optional.
+              </FieldDescription>
+              <Input id='description' name='description' autoComplete='off' />
+            </Field>
+
+            {/* TODO: Add a picture upload feature here */}
+          </FieldGroup>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
     </main>
   );
 }
