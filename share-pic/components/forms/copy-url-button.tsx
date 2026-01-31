2bd5474304141713813774f97aaf1ad6b90e5e4f
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-01-31 11:50:37 +0100

    Add some UX on the copy button

--- share-pic/components/forms/copy-url-button.tsx
+++ share-pic/components/forms/copy-url-button.tsx
@@ -2,20 +2,25 @@
 
 import { Copy } from 'lucide-react';
 import { Button } from '../ui/button';
+import { useState } from 'react';
 
 interface InputParams {
   label?: string;
 }
 
 export default function CopyUrlButton({ label = 'Copy' }: InputParams) {
+  const [copied, setCopied] = useState<boolean>(false);
+
   async function handleCopy() {
+    setCopied(true);
     await navigator.clipboard.writeText(window.location.href);
+    setTimeout(() => setCopied(false), 1500);
   }
 
   return (
-    <Button className='cursor-pointer' onClick={handleCopy}>
+    <Button className='cursor-pointer' onClick={handleCopy} disabled={copied}>
       <Copy />
-      {label}
+      {copied ? 'Copied to clipboard!' : label}
     </Button>
   );
 }
