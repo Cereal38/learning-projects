4bb09f99beb0eabb54078962291e2480bea6cda1
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-07 11:54:33 +0100

    Define a simple carousel for items

--- wardbase/components/item-carousel.tsx
+++ wardbase/components/item-carousel.tsx
@@ -1,15 +1,33 @@
 import { getAllItems } from '@/lib/item';
 import { Item } from '@prisma/client';
 import ItemCard from './item-card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from './ui/carousel';
 
 export default async function ItemCarousel() {
   const items: Item[] = await getAllItems();
 
   return (
-    <div className='flex flex-row gap-4 flex-wrap'>
-      {items.map((item) => (
-        <ItemCard item={item} key={item.id} />
-      ))}
-    </div>
+    // <div className='flex flex-row gap-4 flex-wrap'>
+    //   {items.map((item) => (
+    //     <ItemCard item={item} key={item.id} />
+    //   ))}
+    // </div>
+    <Carousel opts={{ align: 'start' }} className='w-full'>
+      <CarouselContent>
+        {items.map((item) => (
+          <CarouselItem key={item.id} className='basis-auto'>
+            <ItemCard item={item} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
