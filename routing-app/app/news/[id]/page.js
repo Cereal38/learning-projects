690f4c8b25717e26577b7eabc5f8980ebbe23441
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-04-15 16:32:40 +0200

    Update way params are handled

--- routing-app/app/news/[id]/page.js
+++ routing-app/app/news/[id]/page.js
@@ -1,9 +1,5 @@
-'use client';
-
-import { useParams } from 'next/navigation';
-
-export default function NewsDetailsPage() {
-  const { id } = useParams();
+export default function NewsDetailsPage({ params }) {
+  const id = params.id;
 
   return (
     <main>
