ef30fee8059eb2a62de87747ec65115eab57ca18
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-04-15 16:56:27 +0200

    Improve news details page

--- next-news/app/news/[slug]/page.js
+++ next-news/app/news/[slug]/page.js
@@ -2,11 +2,16 @@ import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsDetailsPage({ params }) {
   const slug = params.slug;
-  const newsItem = DUMMY_NEWS.find((item) => item.slug == slug);
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
   return (
-    <main>
-      <h1>{newsItem.title}</h1>
-    </main>
+    <article className='news-article'>
+      <header>
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+      <p>{newsItem.content}</p>
+    </article>
   );
 }
